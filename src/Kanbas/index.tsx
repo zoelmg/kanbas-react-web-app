21d6d3563064520452083f0f4bd62f3fc8c04455
Zoe Gao <122059026+zoelmg@users.noreply.github.com>
2024-03-19 22:34:09 -0400

    a4 State and Redux fa23

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -13,23 +13,62 @@ function Kanbas() {
     _id: "1234", name: "New Course", number: "New Number",
     startDate: "2023-09-10", endDate: "2023-12-15",
   });
+
   const addNewCourse = () => {
+    // check if the course with the same _id already exists
+    const isExisting = courses.some(c => c._id === course._id);
+
+    // if the course already exists, don't add it again because you should only
+    // be able to either edit or delete
+    if (isExisting) {
+      setCourse({
+        _id: "0",
+        name: "Cannot add while editing a course",
+        number: "Please choose again or enter new course",
+        startDate: "2023-09-10",
+        endDate: "2023-12-15",
+     });
+
+        return;
+    }
+
+    // If the course doesn't exist, add it to the courses array
     setCourses([...courses, { ...course, _id: new Date().getTime().toString() }]);
-  };
+
+    setCourse({
+      _id: "0",
+      name: "New Course was successfully added!",
+      number: "New Number",
+      startDate: "2023-09-10",
+      endDate: "2023-12-15",
+   });
+};
+
+
   const deleteCourse = (courseId: any) => {
     setCourses(courses.filter((course) => course._id !== courseId));
   };
+
   const updateCourse = () => {
-    setCourses(
-      courses.map((c) => {
-        if (c._id === course._id) {
-          return course;
-        } else {
-          return c;
-        }
-      })
-    );
-  };
+      setCourses(
+        courses.map((c) => {
+          if (c._id === course._id) {
+            return course;
+          } else {
+            return c;
+          }
+        })
+      );
+        
+    setCourse({
+        _id: "0",
+        name: "Update Success: Enter New Course",
+        number: "Update Success: Enter New Number",
+        startDate: "2023-09-10",
+        endDate: "2023-12-15",
+    });
+};
+
 
   return (
     <Provider store={store}>
